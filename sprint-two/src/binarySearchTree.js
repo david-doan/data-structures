25a5e91fa2a9bd45e6110b1dad86465c389d8bf5
david-doan <a.david.doan@gmail.com>
2016-04-23 12:32:45 -0700

    makes progress on binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,27 @@
 var BinarySearchTree = function(value) {
+  var obj = Object.create(BinarySearchTree.prototype);
+  obj.root = {};
+  obj.root.value = value;
+  obj.root.children = {};
+  obj.root.children.left = null;
+  obj.root.children.right = null;
+  return obj;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  
+};
+
+BinarySearchTree.prototype.contains = function(value, currNode) {
+  var currNode = currNode || this.root;
+  if (currNode.value === value) {
+    return true;
+  } else if ( value < this.root.value ) {
+    currNode.children.left.value === value;
+  }
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {};
 
 /*
  * Complexity: What is the time complexity of the above functions?
